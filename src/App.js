943f96ff5c70a3ff82d5996447858bac308a6f31
omarnero <omarnerou@gmail.com>
2026-06-23 18:58:22 +0300

    fix build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 function App() {
