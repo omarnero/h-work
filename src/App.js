86a68b46b92f8f8741eea3cb5d5f82442b1ce4d0
omarnero <omarnerou@gmail.com>
2026-06-24 16:31:44 +0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
+// import Login from './pages/Login/Login';
 function App() {
 
   return <>
